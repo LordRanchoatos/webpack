4fe986f1bf7624f8087b53d92a2e1f3f37950f94
Atolagbe David Tolulope <atolagbedt@gmail.com>
2024-05-26 00:25:18 +0100

    understanding inners of webpack

---
+++ src/generateJoke.js
@@ -0,0 +1,4 @@
+const generateJoke = () =>
+  "I don't trust stairs, because they are always up to something.";
+
+export default generateJoke;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import generateJoke from './generateJoke';
+
+console.log(generateJoke());
+
+// const jokeBtn = document.getElementById("jokeBtn");
+// jokeBtn.addEventListener("click", generateJoke);We love that you love Bito’s AI Code Completions!
+// You have hit your limit for AI Code Completions per day on your plan.
+// To receive more Completions, please upgrade your plan or contact mailto:support@bito.aiWe love that you love Bito’s AI Code Completions!
+// You have hit your limit for AI Code Completions per day on your plan.
+// To receive more Completions, please upgrade your plan or contact mailto:support@bito.aiWe love that you love Bito’s AI Code Completions!
+// You have hit your limit for AI Code Completions per day on your plan.
+// To receive more Completions, please upgrade your plan or contact mailto:support@bito.ai
