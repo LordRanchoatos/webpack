932dc2d94b05d63b5f5b7e3532e4b96ce045a4c6
Atolagbe David Tolulope <atolagbedt@gmail.com>
2024-05-26 02:56:13 +0100

    webpack gtg!

--- src/generateJoke.js
+++ src/generateJoke.js
@@ -1,4 +1,18 @@
-const generateJoke = () =>
-  "I don't trust stairs, because they are always up to something.";
+import axios from 'axios';
+
+function generateJoke() {
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  };
+
+  axios.get('https://icanhazdadjoke.com', config).then((res) => {
+    document.getElementById('joke').innerHTML = res.data.joke;
+  });
+}
+
+// const generateJoke = () =>
+//   "I don't trust stairs, because they are always up to something.";
 
 export default generateJoke;
