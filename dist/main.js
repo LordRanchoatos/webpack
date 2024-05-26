45f732d7a6ea89383d9242dfe6218680f6c28978
Atolagbe David Tolulope <atolagbedt@gmail.com>
2024-05-26 01:36:28 +0100

    fixed file order issues

--- dist/main.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";console.log("I don't trust stairs, because they are always up to something.")})();
--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
 import generateJoke from './generateJoke';
+import './styles/main.scss';
 
 console.log(generateJoke());
-
-// const jokeBtn = document.getElementById("jokeBtn");
-// jokeBtn.addEventListener("click", generateJoke);We love that you love Bito’s AI Code Completions!
-// You have hit your limit for AI Code Completions per day on your plan.
-// To receive more Completions, please upgrade your plan or contact mailto:support@bito.aiWe love that you love Bito’s AI Code Completions!
-// You have hit your limit for AI Code Completions per day on your plan.
-// To receive more Completions, please upgrade your plan or contact mailto:support@bito.aiWe love that you love Bito’s AI Code Completions!
-// You have hit your limit for AI Code Completions per day on your plan.
-// To receive more Completions, please upgrade your plan or contact mailto:support@bito.ai
